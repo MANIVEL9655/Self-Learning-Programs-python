241c0e0ec335165f1408103cdf5c772849fbcf09
MANIVEL9655 <manivelpgpcet@gmail.com>
2024-11-06 05:34:59 +0530

    Simple Classes and object program created

---
+++ Classes 1.py
@@ -0,0 +1,10 @@
+class mani:
+    def add(self,a,b):
+        print(a+b)
+
+    def sub(self,a,b):
+        print(a-b)
+arithmatic=mani()
+arithmatic1=mani()
+arithmatic.add(12,3)
+arithmatic1.sub(23,4)
