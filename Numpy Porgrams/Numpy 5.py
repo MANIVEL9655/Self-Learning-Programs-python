80831fd84e9b5b91541533e0806e8c43d98486fd
MANIVEL9655 <manivelpgpcet@gmail.com>
2024-11-15 05:20:57 +0530

    Numpy file added

---
+++ Numpy Porgrams/Numpy 5.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.ma.core import array
+
+for i in np.arange(5):
+    print(i)
+print("===="*6)
+
+np_array_2d= np.arange(0,12).reshape(3,4)
+print(np_array_2d)
+sumof_array = np.sum(np_array_2d, axis=0)
+print("#",sumof_array)
+print("==="*4)
+sumof_array = np.sum(np_array_2d, axis=1)
+print("Axis is 1",sumof_array)
