308a9d40c709e444a5dc229b7631f287d5a8e997
MANIVEL9655 <manivelpgpcet@gmail.com>
2024-12-29 11:04:18 +0530

    NewAxis()2

--- Numpy Porgrams/NewAxis() 2.py
+++ Numpy Porgrams/NewAxis() 2.py
@@ -1,6 +1,4 @@
 import numpy as np
-a=np.array([[[[[[[[[[[[[[[[[[[[[[[1,2,3,4]]]]]]]]]]]]]]]]]]]]]]])
-b=(a[:,np.newaxis])
+a=np.array([1,2,3,4])
+b=(a[:,newaxis])
 print(b)
-print(b.ndim)
-print(b.shape)
