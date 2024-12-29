22f1d5bb04a435f5166153a89c7f1fada4b804ee
MANIVEL9655 <manivelpgpcet@gmail.com>
2024-12-29 08:28:48 +0530

    Broadcast 7

---
+++ Numpy Porgrams/BroadCast 7.py
@@ -0,0 +1,17 @@
+import numpy as np
+a=np.arange(1,21).reshape(5,4)
+b=np.arange(2,3).reshape(1)
+print(a)
+print(np.ndim(a))
+print(np.shape(a))
+print("=========================================")
+print(b)
+print(np.ndim(b))
+print(np.shape(b))
+print("==========================================")
+
+results = a*b
+print(results)
+print("======================")
+print(np.ndim(results))
+print(np.shape(results))
