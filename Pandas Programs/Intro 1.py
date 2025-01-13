dfa56d04eb47afd9841ac99369373eba437fc5cd
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-13 22:00:57 +0530

    Intro 2

--- Pandas Programs/Intro 1.py
+++ Pandas Programs/Intro 1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-# import seaborn as sns
 import matplotlib.pyplot as plt
-
 df= pd.DataFrame(data=np.arange(10), dtype=int)
-print(df)
+df.plot.pie(subplots=True)
+plt.show()
