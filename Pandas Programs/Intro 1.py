3f707b2a50785a714bf69682ca762095540c1ccc
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-13 22:04:52 +0530

    Intro 3

--- Pandas Programs/Intro 1.py
+++ Pandas Programs/Intro 1.py
@@ -2,5 +2,4 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 df= pd.DataFrame(data=np.arange(10), dtype=int)
-df.plot.pie(subplots=True)
-plt.show()
+print(df)
--- Pandas Programs/Intro 3.py
+++ Pandas Programs/Intro 3.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 df= pd.DataFrame(data=np.arange(10), dtype=int)
-df.plot.pie(subplots=True)
+df.plot.bar(subplots=True)
 plt.show()
