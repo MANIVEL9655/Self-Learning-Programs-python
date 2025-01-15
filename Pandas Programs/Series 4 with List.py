1648ca5a00553ff82533d17d1fa920ebc0f2c339
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 07:41:28 +0530

    Series 4 with list

--- Pandas Programs/Series 4 with List.py
+++ Pandas Programs/Series 4 with List.py
@@ -4,3 +4,7 @@ lst = [1,2,3,4,5,6]
 indx=[10,20,30,40,50,60]
 s=pd.Series(data=lst,dtype=float,index=indx,copy=True,name="Dataset")
 print(s)
+print(type(lst))
+print(s.ndim)
+print(s.size)
+print(len(s))
