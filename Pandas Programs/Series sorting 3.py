efb0ee9badee8bd6920f365eec8f6f1673bab8a1
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 13:13:13 +0530

    Series sort_value

---
+++ Pandas Programs/Series sorting 3.py
@@ -0,0 +1,10 @@
+from trace import Trace
+
+import pandas as pd
+import numpy as np
+data1= {'a':70,'b':20,'c':100,'d':40,'e':10}
+s=pd.Series(data=data1,index=['b','c','d','e','f','a'])
+print(s)
+print("========================")
+print(s.sort_values(ascending=True,ignore_index=False,inplace=False,axis=0,SortKind = "quicksort"))
+# print(s.sort_index())
---
+++ Pandas Programs/Series.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+data1= {'a':10,'b':20,'c':30,'d':40,'e':50}
+s=pd.Series(data=data1,index=['bb','cb','db','eb','fb','a'])
+print(s)
