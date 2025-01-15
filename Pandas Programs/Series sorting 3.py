1bd175d7a14307d10702f7d671c23f38fec71394
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 13:15:23 +0530

    Series sort_value

--- Pandas Programs/Series sorting 3.py
+++ Pandas Programs/Series sorting 3.py
@@ -6,5 +6,5 @@ data1= {'a':70,'b':20,'c':100,'d':40,'e':10}
 s=pd.Series(data=data1,index=['b','c','d','e','f','a'])
 print(s)
 print("========================")
-print(s.sort_values(ascending=True,ignore_index=False,inplace=False,axis=0,SortKind = "quicksort"))
+print(s.sort_values(ascending=True,ignore_index=False,inplace=False,axis=0))
 # print(s.sort_index())
