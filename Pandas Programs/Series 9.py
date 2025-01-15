ec6b02b5b407a56646069d090d7fa1052a6347e8
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 08:49:54 +0530

    Series with Dict

---
+++ Pandas Programs/Series 9.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+data = np.array(['a','b','c','d','e'])
+s=  pd.Series(data,index=[10,20,30,40,50], copy=True,dtype=str,name="Manivrl")
+print(s)
+
+print(s)
+print(s)
+print(data)
