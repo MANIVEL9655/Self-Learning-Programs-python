7cb7efe03b0a2860cbefde64faa5be35b545f3cb
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 07:26:30 +0530

    Series 2

--- Pandas Programs/Series 2.py
+++ Pandas Programs/Series 2.py
@@ -3,3 +3,7 @@ import numpy as np
 data = np.array(['a','b','c','d','e'])
 s=  pd.Series(data,index=[10,20,30,40,50], copy=True,dtype=str,name="Manivrl")
 print(s)
+s[0]='f'
+print(s)
+print(s)
+print(data)
