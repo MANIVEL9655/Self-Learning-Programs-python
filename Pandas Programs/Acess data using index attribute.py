65e9030b403d10e32648251546578572277bd92b
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-15 14:26:26 +0530

    Series Acess index attributes

--- Pandas Programs/Acess data using index attribute.py
+++ Pandas Programs/Acess data using index attribute.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 s=pd.Series(data= ['a',10,20,'c','d'],index=['b','c','d','e','a'])
 print(s)
-print(s[0])
-print(s[1])
-print(s[2])
-print(s[3])
-print(s[4])
+print(s['b'])
+print(s['e'])
+print(s['c'])
+print(s['a'])
+print(s['d'])
