365743013b30dced67f446990ec2a5fa4533c584
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-28 20:54:11 +0530

    Sorted

--- Pandas Programs/DataFrame/Adding NEw column.py
+++ Pandas Programs/DataFrame/Adding NEw column.py
@@ -1,9 +1,9 @@
 from operator import index
 
 import pandas as pd
-data={'Name':pd.Series(['Kamal','Vishnu','Selva','Rajesh','Yogie'],index=["Row1","Row2","Row3","Row4","Row5"]),"Age":pd.Series(
+data={'Name':pd.Series(sorted(['Kamal','Vishnu','Selva','Rajesh','Yogie']),index=["Row1","Row2","Row3","Row4","Row5"]),"Age":pd.Series(
     [10,20,30,40,50],["Row1","Row2","Row3","Row4","Row5"]
 )}
 df=pd.DataFrame(data=data,copy=True,columns=["Name","Age"])
-df['Marks']=pd.Series([23.4,55.6,56,66,90],index=["Row1","Row2","Row3","Row4","Row5"])
+df['Marks']=pd.Series(sorted([3.4,55.6,5,66,90]),index=sorted(["Row1","Row2","Row3","Row4","Row5"]))
 print(df)
