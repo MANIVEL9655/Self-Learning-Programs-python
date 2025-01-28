be4b82e98f28c32684fdad5bb6a5de9bfd215e0a
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-28 15:06:35 +0530

    DF using dict explicit columns

--- Pandas Programs/DataFrame/Explicit Columns df.py
+++ Pandas Programs/DataFrame/Explicit Columns df.py
@@ -1,4 +1,4 @@
 import pandas as pd
 data=[{"Name":"Ram","AGE":23,"Marks":80},{"Name":"Ram","Marks":70},{"Name":"Ramana","AGE":24,},{"Name":"Ramesh","AGE":34,"Marks":50},{"Name":"Marish","AGE":30,"Marks":40}]
-df=pd.DataFrame(data=data,copy=True,columns=["Name","B","Marks",'AGE'])
+df=pd.DataFrame(data=data,copy=True,columns=["Name","B","Marks"])
 print(df)
