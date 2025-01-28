638c411f0e12459755b90d3baee9ebfddfd2da35
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-28 07:50:11 +0530

    Series props

---
+++ Pandas Programs/Props1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+s=pd.Series([1,2,3,"Name",True,4],index=list("ABCDEF"))
+print(s)
+print("=====================")
+a=s.items()
+print(a)
+print(next(a))
+print("======================")
+b=iter(a)
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
