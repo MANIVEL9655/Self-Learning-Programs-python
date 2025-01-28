541ea7798d7c1686c95b796e056d0c8489e0c37f
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-28 21:32:02 +0530

    iloc() slicing

---
+++ Pandas Programs/DataFrame/iloc() slicing.py
@@ -0,0 +1,11 @@
+from operator import index
+
+import pandas as pd
+data={'One':pd.Series([1,2,3],index=["a","b","c"]),"Two":pd.Series(
+    [1,2,3,4],["a","b","c","d"])}
+df=pd.DataFrame(data=data)
+print(df)
+print("=================================")
+print(df.iloc[0:3])
+print("==================================")
+print(df.loc["a":"d"])
