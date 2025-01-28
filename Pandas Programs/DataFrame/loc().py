7af1e082f0ade8d3991f3cc768186b399f24c305
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-01-28 21:27:30 +0530

    iloc()

---
+++ Pandas Programs/DataFrame/loc().py
@@ -0,0 +1,9 @@
+from operator import index
+
+import pandas as pd
+data={'One':pd.Series([1,2,3],index=["Row1","Row2","Row3"]),"Two":pd.Series(
+    [1,2,3,4],["Row1","Row2","Row3","Row4"])}
+df=pd.DataFrame(data=data)
+print(df)
+print("=================================")
+print(df.loc['Row1'])
