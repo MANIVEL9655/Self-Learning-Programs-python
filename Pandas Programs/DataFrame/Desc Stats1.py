6c1f3821c035e3311d030244d1dee105a260a93d
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-02-12 11:44:00 +0530

    Skip Na

---
+++ Pandas Programs/DataFrame/Desc Stats1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+d={'Name':pd.Series(['Tom','Lily',"King",'Michel','Roshan','Kannan','Karukuty',"Lee","Sambath","Krish","Valli","Naidu"]),
+   'Age':pd.Series([12,34,56,78,90,15,45,66,77,44,33,18]),
+   'Ratings':pd.Series([12,34,55,3,4,2,5,6,0,45,32,1])}
+df=pd.DataFrame(d)
+print(df)
+print("==========================================================")
+print(df.sum(skipna=True))
+print("==========================================================")
+print(df.sum(skipna=False))
