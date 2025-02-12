db8863f6a63b7c6f756dbcb9f43d60c0cd060a12
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-02-12 15:33:11 +0530

    apply() function

--- Pandas Programs/DataFrame/apply().py
+++ Pandas Programs/DataFrame/apply().py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-def adder(ele1,ele2):
+
+def adder(ele1,ele2=3000):
     return ele1+ele2
 df=pd.DataFrame(np.random.randint(40,120,15).reshape(5,3),columns=["col1","col2","col3"])
 print(df)
 print("*"*50)
-print(df["col1"].pipe(adder,10))
+print(df["col1"].apply(adder,ele2=45))
 print(type(df["col1"]))
