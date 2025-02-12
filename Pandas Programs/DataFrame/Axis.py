e93806b43564b42119bf4c9c0f039e1357c13496
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-02-12 14:22:27 +0530

    axis

---
+++ Pandas Programs/DataFrame/Axis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+d={'Name':pd.Series(['Tom','Lily',"King",'Michel','Roshan','Kannan','Karukuty',"Lee","Sambath","Krish","Valli","Naidu"]),
+   'Age':pd.Series([12,34,56,78,90,15,45,66,77,44,33,18]),
+   'Ratings':pd.Series([12,34,55,3,4,2,5,6,0,45,32,1])}
+df=pd.DataFrame(d)
+print(df)
+print("3333333333333333333333333333333333333333333")
+print(df.head(3))
+print("3333333333333333333333333333333333333333333")
