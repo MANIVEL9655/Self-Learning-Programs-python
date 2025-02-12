efc5ae9b66757104610ff26f40633060647d307b
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-02-12 15:36:23 +0530

    apply()2  function

---
+++ Pandas Programs/DataFrame/apply()2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def adder(ele1,marks,grade):
+    return ele1+marks+grade
+    return marks+grade
+df=pd.DataFrame(np.random.randint(40,120,15).reshape(5,3),columns=["col1","col2","col3"])
+print(df)
+print("*"*50)
+print(df.apply(adder,args=(100,200)))
+print(type(df["col1"]))
