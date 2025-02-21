d929dc825ce9103b9e1111dd27451a04af843e0b
MANIVEL9655 <manivelpgpcet@gmail.com>
2025-02-21 21:03:23 +0530

    Linspace() plot?()

--- Pandas Programs/DataFrame/apply().py
+++ Pandas Programs/DataFrame/apply().py
@@ -6,5 +6,5 @@ def adder(ele1,ele2=3000):
 df=pd.DataFrame(np.random.randint(40,120,15).reshape(5,3),columns=["col1","col2","col3"])
 print(df)
 print("*"*50)
-print(df["col1"].apply(adder,ele2=45))
+print(df["col1"].apply(adder,ele2==45))
 print(type(df["col1"]))
